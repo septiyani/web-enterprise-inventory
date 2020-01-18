e76dede2dbca91bc625b2a2cc6b1dd6ec434ad70
MNCSHOP <MNCSHOP@MNCSHOPN00156.mncgroup.com>
2020-01-18 09:12:37 +0700

    add menu for download stock

--- application/views/welcome/welcome_message.php
+++ application/views/welcome/welcome_message.php
@@ -41,46 +41,15 @@
 									<div class="icon">
 										<i class="ion ion-laptop"></i>
 									</div>
-									<a href="<?php echo base_url('inventory/all') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+									<a href="<?php echo base_url('inventory_stock/items') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 								</div>
 							</div>
 							<!-- ./col -->
-							<div class="col-lg-6 col-xs-6">
-								<!-- small box -->
-								<div class="small-box bg-green">
-									<div class="inner">
-										<h3><?php echo $total_category ?></h3>
-
-										<p>Categories</p>
-									</div>
-									<div class="icon">
-										<i class="ion ion-star"></i>
-									</div>
-									<a href="<?php echo base_url('categories') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-								</div>
-							</div>
-							<!-- ./col -->
-							<div class="col-lg-6 col-xs-6">
-								<!-- small box -->
-								<div class="small-box bg-red">
-									<div class="inner">
-										<h3><?php echo $total_location ?></h3>
-
-										<p>Locations</p>
-									</div>
-									<div class="icon">
-										<i class="ion ion-compass"></i>
-									</div>
-									<a href="<?php echo base_url('locations') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-								</div>
-							</div>
+							
 							<!-- ./col -->
 						</div>
 					</div>
-					<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-						<!-- Inventory by category chart -->
-						<canvas id="chart" class="chartjs" width="100%"></canvas>
-					</div>
+					 
 				</div>
 				<!-- /.box-body -->
 				<div class="box-footer">
